33dda0a5f16182b884d225b61537f66d0084f441
Finn Thye <piqueen@gmail.com>
2019-11-12 10:09:42 -0700

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -32,8 +32,36 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  binding.pry
-  director_data.values.sum
+  row_index = 0
+  director_total = 0
+
+  while row_index < director_data[:movies].length do
+    director_total += director_data[:movies][row_index][:worldwide_gross]
+  
+    row_index += 1
+  end
+
+  director_total
 end
 
-#"Jean-Pierre Jeunet" => "222312123123"
+# director_total is this:
+# {:name=>"Stephen Spielberg",
+#   :movies=>
+#    [{:title=>"Jaws", :studio=>"Universal", :worldwide_gross=>260000000, :release_year=>1975},
+#     {:title=>"Close Encounters of the Third Kind",
+#      :studio=>"Columbia",
+#      :worldwide_gross=>135189114,
+#      :release_year=>1977},
+#     {:title=>"Raiders of the Lost Ark",
+#      :studio=>"Paramount",
+#      :worldwide_gross=>248159971,
+#      :release_year=>1981},
+#     {:title=>"E.T. the Extra-terrestrial",
+#      :studio=>"Universal",
+#      :worldwide_gross=>435110554,
+#      :release_year=>1982},
+#     {:title=>"Schindler's List",
+#      :studio=>"Universal",
+#      :worldwide_gross=>96898818,
+#      :release_year=>1993},
+#     {:title=>"Lincoln", :studio=>"Buena Vista", :worldwide_gross=>182207973, :release_year=>2012}]}
